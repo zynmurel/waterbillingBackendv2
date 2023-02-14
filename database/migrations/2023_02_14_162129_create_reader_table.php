28515757e1e8a8d8216eab6e3fdd6279ca58f9eb
zynmurel <comingues99@gmail.com>
2023-02-15 00:26:49 +0800

    change csv for consumers and add migration for reader

---
+++ database/migrations/2023_02_14_162129_create_reader_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reader', function (Blueprint $table) {
+            $table->id('reader_id');
+            $table->integer('user_id');
+            $table->string('first_name', 100);
+            $table->string('middle_name', 100)->nullable();
+            $table->string('last_name', 100);
+            $table->string('gender', 10);
+            $table->integer('birthday');
+            $table->string('phone', 15);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reader');
+    }
+};
